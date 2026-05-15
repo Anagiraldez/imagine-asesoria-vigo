0aa245deef3426a35c4196265b327970aebcc136
ANA <anagiraldez@MacBook-Air-de-ANA.local>
2026-05-15 16:51:54 +0200

    Version final con SEO y limpieza

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   // Si vas a subirlo a una carpeta llamada imagine-asesoramiento, deja la base. 
   // Si es para verlo en tu PC, puedes poner solo '/'
-  base: '/imagine-asesoria-vigo/', 
+  base: '/', 
   plugins: [
     react(),
     tailwindcss(),
